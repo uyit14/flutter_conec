e0960d6a8a900f1abe0d9203db9ad0495f60de7d
uyit14 <uytaibao@gmail.com>
2020-09-04 15:51:13 +0700

    guide screen

--- lib/ui/onboarding/onboarding_screen.dart
+++ lib/ui/onboarding/onboarding_screen.dart
@@ -2,8 +2,41 @@ import 'package:conecapp/ui/conec_home_page.dart';
 import 'package:flutter/material.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
-class OnBoardingScreen extends StatelessWidget {
+class OnBoardingScreen extends StatefulWidget {
   static const ROUTE_NAME = '/onboarding-screen';
+
+  @override
+  _OnBoardingScreenState createState() => _OnBoardingScreenState();
+}
+
+class _OnBoardingScreenState extends State<OnBoardingScreen> {
+  PageController _pageController = PageController(initialPage: 0);
+  int _currentPage = 0;
+
+  void gotoNextPage() {
+    if (_currentPage == 0) {
+      _pageController.animateToPage(
+        1,
+        duration: Duration(milliseconds: 350),
+        curve: Curves.easeIn,
+      );
+    }
+    if (_currentPage == 1) {
+      gotoHome();
+    }
+    setState(() {
+      _currentPage = 1;
+    });
+  }
+
+  void gotoHome() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    prefs.setBool('onboarding', false);
+    Navigator.pushReplacementNamed(
+      context,
+      ConecHomePage.ROUTE_NAME,
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -11,26 +44,41 @@ class OnBoardingScreen extends StatelessWidget {
       child: Scaffold(
         body: Container(
           width: double.infinity,
+          height: double.infinity,
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             crossAxisAlignment: CrossAxisAlignment.center,
             children: <Widget>[
-              Text("Màn hình hướng dẫn sử dụng", style: TextStyle(fontSize: 24)),
-              SizedBox(height: 16),
+              Expanded(
+                child: Stack(
+                  children: [
+                    PageView(
+                      controller: _pageController,
+                      children: [
+                        Image.asset("assets/images/register.jpg",
+                            fit: BoxFit.fill),
+                        Image.asset("assets/images/post.jpg", fit: BoxFit.fill),
+                      ],
+                    ),
+                    Container(
+                      width: double.infinity,
+                      height: double.infinity,
+                      padding: EdgeInsets.symmetric(vertical: 150, horizontal: 32),
+                      color: Colors.black12.withOpacity(0.5),
+                    )
+                  ],
+                ),
+              ),
               Container(
-                width: 250,
+                width: double.infinity,
                 height: 50,
                 child: RaisedButton(
-                  onPressed: () async {
-                    SharedPreferences prefs =
-                        await SharedPreferences.getInstance();
-                    prefs.setBool('onboarding', false);
-                    Navigator.pushReplacementNamed(
-                      context,
-                      ConecHomePage.ROUTE_NAME,
-                    );
-                  },
-                  child: Text("Vào trang chủ"),
+                  color: Colors.red,
+                  onPressed: gotoNextPage,
+                  child: Text(
+                    _currentPage == 1 ? "Vào trang chủ" : "Tiếp theo",
+                    style: TextStyle(fontSize: 18, color: Colors.white),
+                  ),
                 ),
               ),
             ],
